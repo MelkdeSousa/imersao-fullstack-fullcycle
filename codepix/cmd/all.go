50accbeb6a377456f66abc792ab835d622bc0640
MelkdeSousa <melk.sousap@gmail.com>
2021-02-08 12:45:58 -0300

    run server gRPC and Kafka together via CLI

---
+++ codepix/cmd/all.go
@@ -0,0 +1,52 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"github.com/MelkdeSousa/codepix/application/grpc"
+	"github.com/MelkdeSousa/codepix/application/kafka"
+	"github.com/MelkdeSousa/codepix/infrastructure/db"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var (
+	gRPCPortNumber int
+)
+
+// allCmd represents the all command
+var allCmd = &cobra.Command{
+	Use:   "all",
+	Short: "Run gRPC and Kafka Consumer",
+	Run: func(cmd *cobra.Command, args []string) {
+		database := db.ConnectDB(os.Getenv("env"))
+		go grpc.StartGrpcServer(database, portNumber)
+
+		deliveryChan := make(chan ckafka.Event)
+		producer := kafka.NewKafkaProducer()
+		go kafka.DeliveryReport(deliveryChan)
+
+		kafkaProcessor := kafka.NewKafkaProcessor(database, producer, deliveryChan)
+		kafkaProcessor.Consume()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(allCmd)
+
+	allCmd.Flags().IntVarP(&gRPCPortNumber, "grpc-port","p", 500051,"gRPC Port")
+}
